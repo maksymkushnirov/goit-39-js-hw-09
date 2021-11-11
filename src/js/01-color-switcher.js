2328ed30f277e5a3ab47a2a7948053f08017eba6
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-12 01:10:56 +0200

    solve task-01
    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+const timer = {
+  intervalId: null,
+  start() {
+    const startTime = Date.now();
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      if (deltaTime) {
+        document.body.style.background = getRandomHexColor();
+        btnStart.setAttribute('disabled', true);
+      }
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    btnStart.removeAttribute('disabled');
+  },
+};
+
+btnStart.addEventListener('click', timer.start.bind());
+btnStop.addEventListener('click', timer.stop.bind());
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
